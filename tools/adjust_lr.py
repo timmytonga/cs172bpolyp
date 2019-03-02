d4077c06afa1bccdd1b3bd6f1ccaaa7de7c0bc5a
rsivorap <rsivorap@uci.edu>
2019-03-01 20:04:09 -0800

    adjust_lr based on learning rate instead of parameters

--- tools/adjust_lr.py
+++ tools/adjust_lr.py
@@ -1,5 +1,6 @@
-def adjust_learning_rate(optimizer, epoch, args):
+def adjust_learning_rate(optimizer, epoch, lr):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 30))
+    res  = lr * (0.1 ** (epoch // 30))
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        param_group['lr'] = res
+    return res
