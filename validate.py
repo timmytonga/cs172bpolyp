5331bd89c1ea1a31850688aa8f3f512d7351db4b
rsivorap <rsivorap@uci.edu>
2019-03-01 20:57:40 -0800

    Moved Validation Pass to Validate Function

--- validate.py
+++ validate.py
@@ -1,20 +1,31 @@
 from tools import *
 
-def validate(val_loader, model, criterion, args, log):
+def validate(model, validloader, criterion, epoch, log):
 
-    ## Enter Eval Mode
-    model.eval()
-
+    ## Enter Evalution Mode
+    net.eval()
+   
+    ## Validation Pass
+    epoch_valid = []
     with torch.no_grad():
-        for i, data in enumerate(val_loader):
-            train, label = data
+        num_it = len(validloader)
+        for it, data in enumerate(validloader,0):
+            inputs, labels = data
+            inputs = Variable(inputs.cuda())
+            labels = Variable(labels.cuda())
 
-            in_data = train.cuda()
-            out_data = label.cuda()
+            pred = model(inputs)
+            loss = criterion(pred, labels)
 
-            pred = model(in_data)
-            loss = criterion(output, target)
+            temp_valid_loss = loss.item()
+            epoch_valid.append(temp_valid_loss)
+            
+            ## Write Validation Loss
+            log.write('\tValidation: %5.1f      %0.4f  | %0.4f  ... \n' % \
+                        (epoch +(it/num_it), lr, temp_valid_loss))
 
-            # Measure Accuracy
-            valid_acc = 
-            valid_loss = loss.data[0]
+
+        epoch_valid = np.array(epoch_valid)
+        valid_loss = np.mean(epoch_valid)
+    return valid_loss
+
